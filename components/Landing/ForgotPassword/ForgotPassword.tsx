753933dcfecb4c6e9e1e09c1515c124caa5d27b1
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-02-14 22:19:54 +0400

    feat: add forgot password markup

--- components/Landing/ForgotPassword/ForgotPassword.tsx
+++ components/Landing/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,38 @@
+import { NormalInput, useForgotPassword } from "components";
+import { Fragment } from "react";
 import { ForgotPasswordProps } from "./types";
 
 const ForgotPassword: React.FC<ForgotPasswordProps> = ({ setWhichForm }) => {
-  return <div>ForgotPassword</div>;
+  const {} = useForgotPassword();
+
+  return (
+    <Fragment>
+      <h1 className="text-2xl lg:text-3xl text-white font-medium text-center mt-32">
+        Forgot Password?
+      </h1>
+      <form
+        className="flex flex-col w-full items-start my-10 "
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <NormalInput
+          type={"email"}
+          placeholder={"Verification Email"}
+          register={register}
+          errors={errors}
+          name={"email"}
+          message={"Email field is required"}
+          labelText={"Email"}
+        />
+
+        <button
+          type="submit"
+          className="bg-green-600 hover:bg-green-900 transition-all text-white text-2xl w-full py-5 text-center"
+        >
+          Send Email
+        </button>
+      </form>
+    </Fragment>
+  );
 };
 
 export default ForgotPassword;
