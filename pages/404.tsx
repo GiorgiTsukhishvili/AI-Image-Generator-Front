1d6c59daa453e46fcbdc854626df1c2318619a88
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-02-15 18:17:22 +0400

    fix: change text on 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,13 +7,16 @@ const ErrorPage = () => {
   return (
     <Fragment>
       <Head>
-        <title>AI Images</title>
+        <title>404 / Not Found</title>
       </Head>
       <div className="flex flex-col items-center justify-center bg-gray-950 w-full h-screen">
-        <h1 className="text-3xl text-white flex items-center">
-          <ManStick /> Page Not Found 404
+        <h1 className="text-2xl lg:text-3xl text-white flex items-center capitalize text-center">
+          <ManStick /> Page That You Were Looking Gor Was Not Found 404
         </h1>
-        <Link href={"/"} className="text-3xl text-white mt-10 cursor-pointer">
+        <Link
+          href={"/"}
+          className="text-2xl lg:text-3xl text-white mt-10 cursor-pointer"
+        >
           Go To Login
         </Link>
       </div>
