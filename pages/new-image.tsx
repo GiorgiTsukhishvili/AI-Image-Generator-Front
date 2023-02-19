9787b12b7fd0abef307b5b15764bcc83b915d1b5
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-02-19 18:50:23 +0400

    feat: add navigation on new image

--- pages/new-image.tsx
+++ pages/new-image.tsx
@@ -1,5 +1,19 @@
+import { SideNavbar, UserNavbar } from "components";
+import Head from "next/head";
+import { Fragment } from "react";
+
 const NewImage = () => {
-  return <div>NewImage</div>;
+  return (
+    <Fragment>
+      <Head>
+        <title>AI Images / New Image</title>
+      </Head>
+      <div className="bg-gray-950 min-h-screen">
+        <UserNavbar isNewImage={true} />
+        <SideNavbar isNewImage={true} />
+      </div>
+    </Fragment>
+  );
 };
 
 export default NewImage;
